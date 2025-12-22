4b371f36decaa0160093dbffc26ffbdb02574eee
sachin yadav <yadavsachin3166@gmail.com>
2025-12-22 22:50:28 +0530

    Add files via upload

---
+++ age calculator/script.js
@@ -0,0 +1,15 @@
+const birth = document.getElementById("birthday");
+const btn = document.getElementById("btn");
+const result = document.getElementById("result");
+
+btn.addEventListener("click", function(){
+    const birthdayValue = birth.value;
+    if(birthdayValue === ""){
+        result.textContent = "Please enter your birthdate!";
+        return;
+    }
+    const today = new Date();
+    const birthdate = new Date(birthdayValue);
+    let age = today.getFullYear() - birthdate.getFullYear();
+    result.textContent = "Your age is " + age + " year old";
+});
